98e7092e3e55944d32a82bde0c558fdbfb03d514
saturnnx <159318763+saturnnx@users.noreply.github.com>
2024-02-28 14:40:48 -0400

    Add files via upload

---
+++ atividade3caroni/TextFieldTest.java
@@ -0,0 +1,14 @@
+package atividade3caroni;
+
+import javax.swing.JFrame;
+
+public class TextFieldTest
+{
+    public static void main(String[] args)
+    {
+        TextFieldFrame textFieldFrame = new TextFieldFrame();
+        textFieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textFieldFrame.setSize(350,100);
+        textFieldFrame.setVisible(true);
+    }
+}
